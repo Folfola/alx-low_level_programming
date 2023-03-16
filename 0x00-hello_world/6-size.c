a8c2d971530a4af081ea265973376504e7d0424c
salma bensaikouk <salmabensaik@gmail.com>
2023-03-16 06:13:16 -0700

    2 eme push dan cette projet inche allah 200%

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-
-int main() {
-    printf("The size of int is %lu bytes\n", sizeof(int));
-    printf("The size of char is %lu bytes\n", sizeof(char));
-    printf("The size of long is %lu bytes\n", sizeof(long));
-    printf("The size of long long is %lu bytes\n", sizeof(long long));
-    printf("The size of float is %lu bytes\n", sizeof(float));
-    printf("The size of double is %lu bytes\n", sizeof(double));
-    printf("The size of long double is %lu bytes\n", sizeof(long double));
-    printf("The size of void * is %lu bytes\n", sizeof(void *));
-    return 0;
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
+
