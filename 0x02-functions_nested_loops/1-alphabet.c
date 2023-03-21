cc65bb9736a9924b796d47035cbb93fd475e7317
salma bensaikouk <salmabensaik@gmail.com>
2023-03-21 10:28:28 -0700

    c-fonction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
-#include <mai.h>
+#include "main.h"
+
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet - print all alphabet in lowercase
+*/
+
+
 void print_alphabet(void)
 {
 	char letter;
@@ -11,6 +13,7 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 
 	_putchar('\n');
 }
