10459edfa3d370821f802b269563aaa02e378772
salma bensaikouk <salmabensaik@gmail.com>
2023-04-09 07:59:44 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,13 +23,13 @@ int main(int argc, char *argv[])
 		{
 			if (c >= 25)
 				c -= 25;
-			if (c >= 10)
+			else if (c >= 10)
 				c -= 10;
-			if (c >= 5)
+			else if (c >= 5)
 				c -= 5;
-			if (c >= 2)
+			else if (c >= 2)
 				c -= 2;
-			if (c >= 1)
+			else if (c >= 1)
 				c -= 1;
 			coins_num += 1;
 		}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
 				sum += atoi(argv[i]);
 			else
 			{
-				printf("Erroer\n");
+				printf("Error\n");
 				return (1);
 			}
 		}
