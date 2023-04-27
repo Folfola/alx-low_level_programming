d13e8737b0e9799a375b6f8d1122398a3164c11a
salma bensaikouk <salmabensaik@gmail.com>
2023-04-27 11:11:34 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,18 +2,17 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * by Folfola - (Salma)
  * @h: list's name
  * Return:  number of elements
  */
 size_t list_len(const list_t *h)
 {
-	int n = 0;
+	int i = 0;
 
 	while (h)
 	{
-		n++;
+		i++;
 		h = h->next;
 	}
-	return (n);
+	return (i);
 }
