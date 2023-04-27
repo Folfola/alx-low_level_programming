b99ff81361cdf03020dc0524e011d85330743e99
salma bensaikouk <salmabensaik@gmail.com>
2023-04-27 11:02:29 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - befor main
+ * by Folfola - (Salma)
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
