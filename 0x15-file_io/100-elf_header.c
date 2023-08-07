d42e592e12aafa5c14b0b5b0068af11d0a5d249e
salma bensaikouk <salmabensaik@gmail.com>
2023-08-07 05:43:17 -0700

    0x15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,74 +1,84 @@
-#include "main.h"
-#include <elf.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "elf_b.h"
+#include "elf_a.h"
 
 /**
- * main - check the code
- * description: displays the information contained in
- * the ELF header at the start of an ELF file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 98 on failure
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
-int main(int argc, char *argv[])
+void print_version(unsigned char *e_ident)
 {
-	int fd;
-	Elf64_Ehdr *header;
+	printf("  Version:                           %d",
+	       e_ident[EI_VERSION]);
 
-	if (argc != 2)
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
+/**
+ * close_elf - Closes an ELF file.
+ * @elf: The file descriptor of the ELF file.
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO,
+		"Error: Can't close fd %d\n", elf);
+		exit(98);
+	}
+}
+/**
+ * main - Displays the information contained in the
+ *	ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ */
+int main(__silent int argc, char *argv[])
+{
+	Elf64_Ehdr *h;
+	int O, R;
 
-	/* open file */
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]), exit(98);
+	O = open(argv[1], O_RDONLY);
+	if (O == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	h = malloc(sizeof(Elf64_Ehdr));
+	if (h == NULL)
+	{
+		close_elf(O);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	R = read(O, h, sizeof(Elf64_Ehdr));
+	if (R == -1)
+	{
+		free(h);
+		close_elf(O);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
 
-	/* read ELF header */
-	header = malloc(sizeof(Elf64_Ehdr));
-	if (header == NULL)
-		dprintf(STDERR_FILENO, "Error: malloc failed\n"), exit(98);
-	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-	/* check if file is ELF */
-	if (header->e_ident[EI_MAG0] != ELFMAG0 ||
-	    header->e_ident[EI_MAG1] != ELFMAG1 ||
-	    header->e_ident[EI_MAG2] != ELFMAG2 ||
-	    header->e_ident[EI_MAG3] != ELFMAG3)
-		dprintf(STDERR_FILENO, "Error: Not an ELF file\n"), exit(98);
-
-	/* print ELF header */
+	check_elf(h->e_ident);
 	printf("ELF Header:\n");
-	printf("  Magic:   %02x %02x %02x %02x\n",
-	       header->e_ident[EI_MAG0], header->e_ident[EI_MAG1],
-	       header->e_ident[EI_MAG2], header->e_ident[EI_MAG3]);
-	printf("  Class:                             %s\n",
-		   	       header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n",
-		   	       header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n",
-		   	       header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - System V");
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n",
-		   	       header->e_type == ET_EXEC ? "EXEC (Executable file)" : "EXEC (Executable file)");
-	printf("  Entry point address:               0x%lx (bytes into file)\n", header->e_entry);
-	printf("  Start of program headers:          %d (bytes into file)\n", header->e_phoff);
-	printf("  Start of section headers:          %d (bytes into file)\n", header->e_shoff);
-	printf("  Flags:                             0x%x\n", header->e_flags);
-	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
-	printf("  Number of program headers:         %d\n", header->e_phnum);
-	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
-	printf("  Number of section headers:         %d\n", header->e_shnum);
-	printf("  Section header string table index: %d\n", header->e_shstrndx);
+	print_magic(h->e_ident);
+	print_class(h->e_ident);
+	print_data(h->e_ident);
+	print_version(h->e_ident);
+	print_osabi(h->e_ident);
+	print_abi(h->e_ident);
+	print_type(h->e_type, h->e_ident);
+	print_entry(h->e_entry, h->e_ident);
 
-	/* close file */
-	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(98);
-
+	free(h);
+	close_elf(O);
 	return (0);
 }
